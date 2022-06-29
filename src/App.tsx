cbaf4e4e26157cbc0e94ba05d7dc3d45a6da096d
Mir <miminari13@gmail.com>
2022-06-29 20:02:16 +0300

    Init app

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css' // to use bootstrap.css
+import './assets/scss/app.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app__header">
+        Header
+      </div>
+      Body
+    </div>
+  );
+}
+
+export default App;
